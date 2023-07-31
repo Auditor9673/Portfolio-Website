7cd6a94e0891b64e3f8bf3c93776dcd53cd3e41a
Auditor9673 <112822602+Auditor9673@users.noreply.github.com>
2023-07-31 06:29:34 +0530

    idk

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -15,4 +15,4 @@ From: {user_email}
     button = st.form_submit_button("Submit")
     if button:
         send_email(message)
-        st.info("Your email is successfully sent !")
+        st.info("Your message is successfully sent !")
